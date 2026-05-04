7ddb467b6befa825554f460b76e2923c06434c58
leandrotcawork <leandrotca.work@gmail.com>
2026-05-03 22:12:35 -0300

    feat(api): E4 add apiFetch shared HTTP client with 401 dispatch

---
+++ frontend/apps/web/src/lib/api/index.ts
@@ -0,0 +1,4 @@
+﻿export { apiFetch } from "./client";
+export { ApiError, resolveErrorMessage } from "./errors";
+export { dispatchAuthExpired, onAuthExpired, AUTH_EXPIRED_EVENT } from "./authBus";
+export type { AuthExpiredDetail } from "./authBus";
